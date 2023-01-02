0d21982a6ce3a6b755129d531147d991ddfcd64e
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-01-02 12:50:10 +0530

    Day04

---
+++ Typescript/Day04/demo1.js
@@ -0,0 +1,41 @@
+// console.log("Welcome to angular 12");
+// console.log('Hopes so u r enjoy');
+// //back tick operator
+// console.log(`U r studying typescript`)
+/*
+
+*/
+//variable
+var a1;
+var a2, a3, a4;
+var a$b, a_b;
+// data types
+// number(int,float,double,short,long) ,string(''/""/``),boolean(true/false)
+// any (number,string,boolean)
+// void (neutral data data type => it doesn not point any thing)
+// misce. null & undefined => it store data type as well as value 
+var a = 10; // forward declaration & defination 
+// console.log('Value of a is '+a);
+// console.log("Value of a is "+a);
+// console.log(`Value of a is ${a} `);
+var str;
+// console.log("Value of str is "+str);
+// str='Sumit raokhande';
+// console.log('Value of str is '+str)
+var active = true;
+// console.log("Active is "+active)
+var myany;
+myany = 2.5;
+console.log("Value of myany " + myany);
+myany = "Angular 12";
+console.log("Value of myany " + myany);
+myany = false;
+console.log("Value of myany " + myany);
+var b = 4.5;
+b = 40;
+// b=''
+var c;
+c = 'Sumit';
+c = 2.5;
+c = true;
+console.log("Value of c " + c);
---
+++ Typescript/Day04/demo1.ts
@@ -0,0 +1,80 @@
+// console.log("Welcome to angular 12");
+// console.log('Hopes so u r enjoy');
+// //back tick operator
+// console.log(`U r studying typescript`)
+
+/*
+
+*/
+
+//variable
+
+var a1;
+var a2,a3,a4;
+var a$b,a_b;
+
+// data types
+// number(int,float,double,short,long) ,string(''/""/``),boolean(true/false)
+// any (number,string,boolean)
+// void (neutral data data type => it doesn not point any thing)
+// misce. null & undefined => it store data type as well as value 
+
+    var a:number=10; // forward declaration & defination 
+    // console.log('Value of a is '+a);
+    // console.log("Value of a is "+a);
+    // console.log(`Value of a is ${a} `);
+
+    var str!:string;
+    // console.log("Value of str is "+str);
+    // str='Sumit raokhande';
+    // console.log('Value of str is '+str)
+
+
+    var active:boolean=true;
+    // console.log("Active is "+active)
+
+    var myany:any;
+    myany=2.5;
+    console.log("Value of myany "+myany);
+    myany="Angular 12";
+    console.log("Value of myany "+myany);
+    myany=false;
+    console.log("Value of myany "+myany);
+
+    var b=4.5;
+    b=40;
+    // b=''
+     var c;
+    // c='Sumit';
+    // c=2.5;
+    // c=true;
+    // console.log("Value of c "+c)
+
+
+    //Type Assertion 
+    var c1:any;
+    // any,object,unknown
+    // 1 angle bracket
+        // used in typescript file
+    // 2 as syntax
+        // ts file as well as  html file 
+
+      // 1 angle bracket
+        var temp=(<string> c1);
+        // temp.
+        // (<string> c1).
+        
+      // 2 as syntax
+      var temp2=(c1 as string);
+    //   temp2.
+        
+
+
+
+    
+
+
+
+
+
+
