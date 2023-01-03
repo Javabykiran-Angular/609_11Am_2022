1d19e4e7cc248037871746e8dcac2f32938ece4e
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-01-03 12:53:54 +0530

    Day05

---
+++ Typescript/Day05/demo1.ts
@@ -0,0 +1,71 @@
+//operators
+//Airthamthic => +,-,*,/,%
+//Logical => &&,||,
+//bitwise => !,~,^,|,&,>>,<<
+//conditional => <,>,==(it checks only value ),=== (Strongly equality => it checks value as well as data type )
+//relational => <=,>=,!=
+//ternary => condition?expression1:expresssion2
+//unary => inc/dec post/pre ++a,a--
+//assignment => =,+=,-=,/=,*=,%=
+
+//control statement
+// if,if-else,switch ,break,continue,if-else ladder
+
+// var count=2;
+// if(count<4){
+//     console.log("Condition is true ")
+// }else{
+//     console.log("Condition is false ")
+// }
+
+var choice=21
+// switch(choice){
+//     case 1: console.log("U r in case 1")
+//             break
+//     case 2 : console.log("U r in case 2 ")
+//             break
+//     default: console.log("U r in default case ")
+//             break
+// }
+
+
+//loop statement
+// while,do-while,for,foreach
+//while
+var count=5;
+// while(count!=0){
+//     console.log("Count is "+count);
+//     count--;
+// }
+
+// do{
+//     console.log("Count is===== "+count);
+//     count--;
+// }while(count!=0);
+
+// for(var i=0;i<4;i++){
+//     // console.log(" "+"*");
+//     console.log("value of i "+i);
+// }
+// console.log("Value of i is "+i)
+// var => it is global scope 
+
+// let -> keywords 
+// scope => within nearest block ( {} ) 
+
+// for(let j=0;j<4;j++){
+//     console.log("Value of j "+j);        
+// }
+// console.log("access j after loop "+j)
+
+// const => keyword
+// scope => global as well as local scope 
+
+const pi:number=3.14;
+// const pi1:string='';
+
+
+
+
+
+ 
---
+++ Typescript/Day05/demo2.ts
@@ -0,0 +1,42 @@
+// add1();
+//Basic user defined functions types 
+
+// 1 function without paramtere & without return type
+// 2 function with paramtere & without return type
+// 3 function without paramtere & with return type
+// 4 function with paramtere & with return type
+
+// 1 function without paramtere & without return type
+
+    function add1(){
+        console.log("U r in first type function")
+    }
+
+    // 2 function with paramtere & without return type
+
+   function add2(a:number,b:number){
+        console.log("Addition is "+(a+b));
+   }
+
+//    add2(5,6);
+
+// 3 function without paramtere & with return type
+
+   function add3():number{
+        return (5+5);
+   }
+
+//    let res= add3();
+//    console.log("Result is "+res)
+
+
+// 4 function with paramtere & with return type
+
+   function add4(a:number,b:number):number{
+        return (a+b);
+   }
+
+   let temp=add4(7,3);
+   console.log("Addition is "+temp);
+
+   
