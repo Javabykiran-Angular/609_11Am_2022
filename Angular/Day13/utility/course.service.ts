2c5592a5f345da420b9daaa3d09bb1f495f9159a
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-02-03 13:12:44 +0530

    Day13

---
+++ Angular/Day13/utility/course.service.ts
@@ -0,0 +1,10 @@
+
+export class CourseService{
+
+    private course:string[]=['Core java','Advanced java','Spring Boot','JSP','Angular 12','AWS'];
+
+    getCourse(){
+        return (this.course);
+    }
+
+}
